fa45a5fd7aafd8ac189381c30708ac9cf9296a27
Meharaz Hossain <59823440+meharaz2020@users.noreply.github.com>
2023-12-13 16:29:42 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,39 @@
+
 import dash
 from dash import dcc, html
 import plotly.express as px
 import pandas as pd
 
-# Sample data (you can replace this with your own dataset)
-df = pd.DataFrame({
-    "X": [1, 2, 3, 4, 5],
-    "Y": [10, 15, 13, 18, 20]
-})
+url = 'https://raw.githubusercontent.com/meharaz2020/ex/master/CM.xlsx'
 
+# Read Excel file from the URL
+df = pd.read_excel(url)
+# Calculating grouped data for the pie charts
+grouped_df = df.groupby('Payment Status')['SEX'].value_counts().reset_index(name='Count')
+amount_per_package = df.groupby('Package Name')['Price'].sum().reset_index()
+
+# Initializing the Dash app
 app = dash.Dash(__name__)
-# Dash app layout and callbacks...
-server = app.server  # This exposes the server attribute for Gunicorn
-
-
+#server = app.server
+# Creating layout for the dashboard
 app.layout = html.Div([
-    html.H1("Plotly Dash Dashboard"),
-    dcc.Graph(
-        id='scatter-plot',
-        figure=px.scatter(df, x='X', y='Y', title='Scatter Plot')
-    )
+    html.H1("Dashboard For Candidate Monitization"),
+    
+    html.Div([
+        dcc.Graph(figure=px.pie(amount_per_package, values='Price', names='Package Name', title='Total Amount per Package Name'))
+    ], style={'width': '30%', 'display': 'inline-block'}),
+    
+    html.Div([
+        dcc.Graph(figure=px.pie(grouped_df, values='Count', names='SEX', title='Total F/M per Payment Status'))
+    ], style={'width': '30%', 'display': 'inline-block'}),
+    
+    html.Div([
+        dcc.Graph(figure=px.bar(grouped_df, x='Payment Status', y='Count', color='SEX',
+                                 title='Payment Type Wise Gender Distribution',
+                                 labels={'Payment Status': 'Payment Type', 'Count': 'Count of Gender'}))
+    ], style={'width': '40%', 'display': 'inline-block'})
 ])
 
+# Running the app
 if __name__ == '__main__':
     app.run_server(debug=False)
