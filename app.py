72aa4839ff7adec3deeeb4d2558adcc533711076
Meharaz Hossain <59823440+meharaz2020@users.noreply.github.com>
2023-12-14 11:10:59 +0600

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ df = pd.read_excel(url)
 
 # Calculate CM day wise subscriptions
 df['Purchase Date'] = pd.to_datetime(df['Purchase Date'])
+# Calculate total earnings where Payment Status is 'S'
+total_earnings = df[df['Payment Status'] == 'S']['Price'].sum()
+
+# Format the total earnings to include only the BDT symbol
+total_earnings_formatted = f"৳{total_earnings:,.0f}"  # Assuming 'total_earnings' is in integer format
 
 # Initialize the Dash app
 app = dash.Dash(__name__)
@@ -20,6 +25,9 @@ server = app.server
 app.layout = html.Div([
     html.H1("Dashboard For Candidate Monetization", style={'textAlign': 'center'}),
     
+    html.Div([
+        html.H3(f"Total Earnings : {total_earnings_formatted}", style={'textAlign': 'center', 'margin-bottom': '20px'})
+    ]),
     dcc.DatePickerRange(
         id='date-range-picker',
         min_date_allowed=df['Purchase Date'].min(),
@@ -33,14 +41,20 @@ app.layout = html.Div([
     
     dcc.Graph(id='amount-per-package'),
     dcc.Graph(id='gender-per-payment'),
-    dcc.Graph(id='payment-gender-distribution')
+    dcc.Graph(id='payment-gender-distribution'),
+    dcc.Graph(id='price-per-payment-status'),
+    dcc.Graph(id='price-per-package'),
+    dcc.Graph(id='price-per-method') 
 ])
 
 # Callback to update the graphs based on the selected date range
 @app.callback(
     [Output('amount-per-package', 'figure'),
      Output('gender-per-payment', 'figure'),
-     Output('payment-gender-distribution', 'figure')],
+     Output('payment-gender-distribution', 'figure'),
+     Output('price-per-payment-status', 'figure'),
+     Output('price-per-package', 'figure'),  # New output for Package Name wise total price
+     Output('price-per-method', 'figure')],  # New output for Package Name wise total price
     [Input('date-range-picker', 'start_date'),
      Input('date-range-picker', 'end_date')]
 )
@@ -56,7 +70,25 @@ def update_graphs(start_date, end_date):
                                              title='Payment Type Wise Gender Distribution',
                                              labels={'Payment Status': 'Payment Type', 'Count': 'Count of Gender'})
     
-    return fig_amount_per_package, fig_gender_per_payment, fig_payment_gender_distribution
+    # New graph calculation for Price per Payment Status
+    price_per_payment_status = filtered_df.groupby('Payment Status')['Price'].sum().reset_index()
+    fig_price_per_payment_status = px.bar(price_per_payment_status, x='Payment Status', y='Price',
+                                          title='Total Price per Payment Status',
+                                          labels={'Payment Status': 'Payment Status', 'Price': 'Total Price'})
+    
+    # New graph calculation for Price per Package Name
+    price_per_package = filtered_df.groupby('Package Name')['Price'].sum().reset_index()
+    fig_price_per_package = px.bar(price_per_package, x='Package Name', y='Price',
+                                   title='Total Price per Package Name',
+                                   labels={'Package Name': 'Package Name', 'Price': 'Total Price'})
+     # New graph calculation for Price per Payment Method
+
+    price_per_Method = filtered_df.groupby('Payment Method')['Price'].sum().reset_index()
+    fig_price_per_Method = px.bar(price_per_Method, x='Payment Method', y='Price',
+                                   title='Total Price per Payment Method',
+                                   labels={'Payment Method': 'Payment Method', 'Price': 'Total Price'})
+    
+    return fig_amount_per_package, fig_gender_per_payment, fig_payment_gender_distribution, fig_price_per_payment_status, fig_price_per_package,fig_price_per_Method
 
 # Running the app
 if __name__ == '__main__':
